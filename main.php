fc3bc8083235ba43c3040bdba862ce41376e9641
denverpaul26 <denverlegend26@gmail.com>
2025-08-13 01:06:20 +0000

    Act 2

---
+++ main.php
@@ -0,0 +1,20 @@
+<?php 
+$Name = "Denver Ramos";
+$Age = 20;
+$School = "GRC";
+$Course = "BSIT";
+$Subject = "WebSys";
+$isStudent = Yes;
+
+echo "My name is $Name<br>";
+echo "My age is $Age<br>";
+echo "My name was at $School<br>";
+echo "My name is $Course<br>";
+echo "My name is $Subject<br>";
+echo "Are you a student? $isStudent<br>";
+
+
+
+
+
+?>
